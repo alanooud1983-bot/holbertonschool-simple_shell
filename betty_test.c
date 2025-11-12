2ecbf87e1638db0911cece0de45d33227f2f25ae
root <root@ip-10-42-35-203.ec2.internal>
2025-11-12 20:55:24 +0000

    Add Betty clean file for Task 1

---
+++ betty_test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Simple program that prints a message
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, Betty!\n");
+	return (0);
+}
+
