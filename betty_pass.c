698e961038229d099700368df91f0bf89066d7cc
root <root@ip-10-42-35-203.ec2.internal>
2025-11-12 21:00:59 +0000

    Betty passing file for Task 1

---
+++ betty_pass.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Simple program to pass Betty checks
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Betty would be proud!\n");
+	return (0);
+}
